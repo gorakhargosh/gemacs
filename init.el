a392e13794975656a9e8d9b00466babe266427f4
Yesudeep Mangalapilly <yesudeep@google.com>
2012-07-21 01:02:39 +0530

    Attempt at getting mouse to work with iterm2 and emacs.

--- init.el
+++ init.el
@@ -446,15 +446,19 @@
 ;; Scroll faster.
 (setq mouse-wheel-scroll-amount '(7 ((shift) . 1) ((control) . nil)))
 
-;; Enables mouse scrolling when using Emacs in the terminal.
+;; Enables mouse support scrolling when using Emacs in the terminal.
 (unless window-system
-  (xterm-mouse-mode 1)
+  (require 'mouse)
+  (xterm-mouse-mode t)
   (global-set-key [mouse-4] '(lambda ()
                                (interactive)
                                (scroll-down 1)))
   (global-set-key [mouse-5] '(lambda ()
                                (interactive)
-                               (scroll-up 1))))
+                               (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+  )
 
 ;; ----------------------------------------------------------------------
 ;; Editing
