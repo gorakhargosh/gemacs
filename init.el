f900a1fec730d4f5bfbddf4aff4006eb4fbc446d
Yesudeep Mangalapilly <yesudeep@google.com>
2012-07-20 14:35:23 +0530

    license.

--- init.el
+++ init.el
@@ -1,13 +1,28 @@
 ;;; init.el -- Emacs configuration in a single file.
 ;;
-;; Copyright 2012 Google Inc. All Rights Reserved.
+;; Copyright (C) 2012  Google Inc.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;; Author: yesudeep@google.com (Yesudeep Mangalapilly)
 ;;
 ;; Description:
 ;;   All of the configuration has been placed into a single file
 ;;   to load it extremely quickly even when using NFS.
-
+;;
+;;; This file is NOT a part of GNU Emacs.
+;;
 
 (require 'cl)
 
