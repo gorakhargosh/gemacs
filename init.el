9a23014a37740a964a32919ab717354655323429
Yesudeep Mangalapilly <yesudeep@google.com>
2012-07-25 06:32:29 +0530

    ido + recentf. Thanks to:
    http://www.masteringemacs.org/articles/2011/01/27/find-files-faster-recent-files-package/

--- init.el
+++ init.el
@@ -523,8 +523,17 @@
 (require 'recentf)
 ;; Disable before we start recentf for tramp.
 (setq recentf-auto-cleanup 'never)
-(recentf-mode 1)
-
+(recentf-mode t)
+(setq recentf-max-saved-items 50)
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
+;; get rid of `find-file-read-only' and replace it with something
+;; more useful.
+(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
 
 ;; ======================================================================
 ;; goog/ido
