d206a3fe9c3bb170c60ef4ddd3b5def8bd5663a1
Yesudeep Mangalapilly <yesudeep@google.com>
2012-07-25 19:26:50 +0530

    Use tango-2-theme.

--- user/mohanl.el
+++ user/mohanl.el
@@ -1,6 +1,7 @@
 (progn
   (setq user-full-name "Mohan Lal"
         user-mail-address "mohanl@google.com")
-  (require 'monokai-theme)
+  ;; (require 'monokai-theme)
+  (require 'tango-2-theme)
   )
 (provide 'mohanl)
--- user/yesudeep.el
+++ user/yesudeep.el
@@ -2,6 +2,7 @@
   (setq user-full-name "Yesudeep Mangalapilly"
         user-mail-address "yesudeep@google.com")
   ;; (require 'monokai-theme)
-  (require 'github-theme)
+  ;; (require 'github-theme)
+  (require 'tango-2-theme)
   )
 (provide 'yesudeep)
