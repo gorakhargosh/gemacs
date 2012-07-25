84c5bfe1108c29099a5ac1008a959839f9c6ae71
Yesudeep Mangalapilly <yesudeep@google.com>
2012-07-25 19:43:02 +0530

    Adds shubhangi's configuration.

---
+++ user/shubhangi.el
@@ -0,0 +1,7 @@
+(progn
+  (setq user-full-name "Shubhangi Taneja"
+        user-mail-address "shubhangi@google.com")
+  ;; (require 'monokai-theme)
+  (require 'tango-2-theme)
+  )
+(provide 'shubhangi)
