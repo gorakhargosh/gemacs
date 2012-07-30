5d04ce3e0f2b26feabff1644f8bcb9e997132579
Yesudeep Mangalapilly <yesudeep@google.com>
2012-07-31 01:43:30 +0530

    Comment out what I really really really mean to.

--- init.el
+++ init.el
@@ -1069,6 +1069,22 @@ immediately."
 ;; Highlight watchwords.
 (add-hook 'prog-mode-hook 'goog/edit/watchwords)
 
+;; Comment what I really really really mean.
+;; Original idea from
+;; http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+If no region is selected and current line is not blank and we are
+not at the end of the line, then comment current line. Replaces
+default behaviour of comment-dwim, when it inserts comment at the
+end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'comment-dwim-line)
+
 ;; ----------------------------------------------------------------------
 ;; sh-mode
 ;; ----------------------------------------------------------------------
