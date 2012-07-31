278eee4be3bc50361b7489353ccd9cbba2903ed2
Yesudeep Mangalapilly <yesudeep@google.com>
2012-07-31 10:16:56 +0530

    Clean up.

--- init.el
+++ init.el
@@ -1253,9 +1253,13 @@ compilation output."
 (add-hook 'python-mode-hook 'goog/config/python-mode/setup-style)
 
 (font-lock-add-keywords 'python-mode
-    '(("\\<\\(object\\|str\\|else\\|except\\|finally\\|try\\|\\)\\>" 0 py-builtins-face)  ; adds object and str and fixes it so that keywords that often appear with : are assigned as builtin-face
-    ("\\<[\\+-]?[0-9]+\\(.[0-9]+\\)?\\>" 0 'font-lock-constant-face) ; FIXME: negative or positive prefixes do not highlight to this regexp but does to one below
-    ("\\([][{}()~^<>:=,.\\+*/%-]\\)" 0 'widget-inactive-face)))
+    '(;; Adds object and str and fixes it so that keywords that often appear
+      ;; with : are assigned as builtin-face
+      ("\\<\\(object\\|str\\|else\\|except\\|finally\\|try\\|\\)\\>" 0 py-builtins-face)
+      ;; FIXME: negative or positive prefixes do not highlight to this regexp
+      ;; but does to one below
+      ("\\<[\\+-]?[0-9]+\\(.[0-9]+\\)?\\>" 0 'font-lock-constant-face)
+      ("\\([][{}()~^<>:=,.\\+*/%-]\\)" 0 'widget-inactive-face)))
 
 ;; ======================================================================
 ;; Keyboard bindings.
