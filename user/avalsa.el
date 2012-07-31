662b4da51fa3ef189e408c8aa49b4112ba96cdff
Yesudeep Mangalapilly <yesudeep@google.com>
2012-07-31 11:05:04 +0530

    Adds avalsa's configuration.

---
+++ user/avalsa.el
@@ -0,0 +1,10 @@
+(progn
+  (setq user-full-name "Akshay Valsa"
+        user-mail-address "avalsa@google.com")
+  (when window-system
+    (require 'burp-theme)
+    ;; (require 'cloud-theme)
+    ;; (require 'google-code-theme)
+    )
+  )
+(provide 'avalsa)
