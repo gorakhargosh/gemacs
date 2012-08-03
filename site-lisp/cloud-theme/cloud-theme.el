77f3e6b96c0b1a0cc83eaa2059d10c53dffa2adf
Yesudeep Mangalapilly <yesudeep@google.com>
2012-08-03 23:19:25 +0530

    Adds author email.

--- site-lisp/cloud-theme/cloud-theme.el
+++ site-lisp/cloud-theme/cloud-theme.el
@@ -17,7 +17,7 @@
 ;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 ;; MA 02111-1307 USA
 ;;
-;; Author: Yesudeep Mangalapilly
+;; Author: Yesudeep Mangalapilly <yesudeep@google.com>
 ;; Version: 1.0.0
 
 (deftheme cloud
