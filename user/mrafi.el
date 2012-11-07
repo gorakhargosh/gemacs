78dfcdd28d725b20447064b5599afb0c79ad0e57
Yesudeep Mangalapilly <yesudeep@google.com>
2012-11-07 11:24:12 +0530

    Adds rafi's configuration.

---
+++ user/mrafi.el
@@ -0,0 +1,10 @@
+(progn
+  (setq user-full-name "Mohammad Rafi"
+        user-mail-address "mrafi@google.com")
+  (when window-system
+    ;; (require 'monokai-theme)
+    ;; (require 'github-theme)
+    ;; (require 'tango-2-theme)
+    (require 'burp-theme)
+  ))
+(provide 'mrafi)
