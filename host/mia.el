edb96f7429aeaac5a443f24a0e0cf9e37423dd7d
Yesudeep Mangalapilly <yesudeep@google.com>
2012-11-28 22:19:55 +0530

    Configure mia local mac os x specific path just in case.

--- host/mia.el
+++ host/mia.el
@@ -1,9 +1,8 @@
 (progn
   ;; (when window-system
   ;;   (when (goog/platform/is-darwin-p)
-  ;;     ;; Monaco is clean. The default is too small in the GUI.
-  ;;     (set-face-font 'default "Consolas-16")
-  ;;     ;; Mac OS X-specific font anti-aliasing.
-  ;;     (setq mac-allow-anti-aliasing t)))
+  ;;     (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+  ;;     (setq exec-path (append exec-path '("/usr/local/bin")))
+  ;;     ))
   )
 (provide 'mia)
