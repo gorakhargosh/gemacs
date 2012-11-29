1425aa17b778526c74e9262a5da82872d1399463
Yesudeep Mangalapilly <yesudeep@google.com>
2012-11-29 20:48:49 +0530

    Set the path anyway.

--- host/mia.el
+++ host/mia.el
@@ -1,8 +1,8 @@
 (progn
-  ;; (when window-system
-  ;;   (when (goog/platform/is-darwin-p)
-  ;;     (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-  ;;     (setq exec-path (append exec-path '("/usr/local/bin")))
-  ;;     ))
+  (when window-system
+    (when (goog/platform/is-darwin-p)
+      (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+      (setq exec-path (append exec-path '("/usr/local/bin")))
+      ))
   )
 (provide 'mia)
