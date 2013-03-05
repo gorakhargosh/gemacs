73452518cc56f6d013d73ee8e898ad28dcac024f
Yesudeep Mangalapilly <yesudeep@google.com>
2013-03-05 09:09:51 +0530

    New user.

---
+++ user/anshukumar.el
@@ -0,0 +1,10 @@
+(progn
+  (setq user-full-name "Anshu Kumar"
+        user-mail-address "anshukumar@google.com")
+  (when window-system
+    (require 'burp-theme)
+    ;; (require 'cloud-theme)
+    ;; (require 'google-code-theme)
+    )
+  )
+(provide 'anshukumar)
