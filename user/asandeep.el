7705d8b945ad6b82f74827cc6ea07c1ac580024b
Yesudeep Mangalapilly <yesudeep@gmail.com>
2014-12-10 09:44:18 +0530

    Adds configuration for Sandeep.

---
+++ user/asandeep.el
@@ -0,0 +1,18 @@
+(progn
+  (setq user-full-name "Sandeep Aggarwal"
+        user-mail-address "asandeep@google.com")
+  (when window-system
+
+    ;; Transparency. Set this before setting up your theme. The theme may
+    ;; configure itself to use better colors for transparency.
+    ;; (set-frame-parameter (selected-frame) 'alpha '(<aemctive> [<inactive>]))
+    ;; (set-frame-parameter (selected-frame) 'alpha '(85 70))
+    ;; (add-to-list 'default-frame-alist '(alpha 85 70))
+
+    ;; (require 'monokai-theme)
+    ;; (require 'github-theme)
+    ;; (require 'tango-2-theme)
+    ;; (require 'burp-theme)
+    )
+  )
+(provide 'asandeep)
