e12c4b7ad3546c60abf2d456bfa13a54053b2fab
Yesudeep Mangalapilly <yesudeep@gmail.com>
2014-12-21 18:19:40 +0530

    Reorg css.

--- init.el
+++ init.el
@@ -926,34 +926,7 @@ immediately."
 (add-hook 'html-mode-hook 'goog/config/html-mode/setup)
 
 
-;; ----------------------------------------------------------------------
-;; CSS mode.
-;; ----------------------------------------------------------------------
-
-;; (require 'less-css-mode)
-(defun goog/config/css-mode/setup ()
-  "Configures `css-mode'."
-
-  ;; Coding style.
-  (setq less-css-indent-level 2
-        css-indent-offset 2
-        indent-tabs-mode nil
-        require-final-newline 't
-        tab-width 2)
-
-  ;; Autocomplete.
-  (setq ac-sources '(
-                     ac-source-words-in-buffer
-                     ac-source-abbrev
-                     ac-source-symbols
-                     ac-source-variables
-                     ac-source-words-in-same-mode-buffers
-                     ac-source-yasnippet
-                     ac-source-css-property
-                     )))
-(add-hook 'css-mode-hook 'goog/config/css-mode/setup)
-;; (add-hook 'less-css-mode-hook 'goog/config/css-mode/setup)
-
+(require 'gemacs-css)
 (require 'gemacs-dart)
 (require 'gemacs-go)
 (require 'gemacs-javascript)
