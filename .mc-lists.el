6745869580c3a1544b96aa303b2f7ecefc98426d
Yesudeep Mangalapilly <yesudeep@google.com>
2015-02-11 22:38:45 +0530

    Updates keyboard binding for duplication.

--- .mc-lists.el
+++ .mc-lists.el
@@ -4,6 +4,7 @@
 (setq mc/cmds-to-run-for-all
       '(
         electric-graphviz-dot-semi
+        electric-newline-and-maybe-indent
         sp-remove-active-pair-overlay
         yaml-electric-backspace
         yaml-electric-dash-and-dot
