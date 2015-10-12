252dd936ee498b9e4b87b4f92e18a6a7a47538ea
Yesudeep Mangalapilly <yesudeep@google.com>
2015-10-12 23:16:55 +0530

    Yaar.

--- snippets/python-mode/.yas-setup.el
+++ snippets/python-mode/.yas-setup.el
@@ -4,6 +4,20 @@
              (split-string x "[[:blank:]]*=[[:blank:]]*" t))
           (split-string arg-string "[[:blank:]]*,[[:blank:]]*" t)))
 
+(defun python-args-to-google-docstring ()
+  "return docstring format for the python arguments in yas-text"
+  (let* ((indent (concat "\n" (make-string (current-column) 32)))
+         (args (python-split-args yas-text))
+         (max-len (if args (apply 'max (mapcar (lambda (x) (length (nth 0 x))) args)) 0))
+         (formatted-args (mapconcat
+                (lambda (x)
+                   (concat (nth 0 x) ": "
+                           (if (nth 1 x) (concat "\(default " (nth 1 x) "\)"))))
+                args
+                indent)))
+    (unless (string= formatted-args "")
+      (mapconcat 'identity (list "Keyword Arguments:" formatted-args) indent))))
+
 (defun python-args-to-docstring ()
   "return docstring format for the python arguments in yas-text"
   (let* ((indent (concat "\n" (make-string (current-column) 32)))
