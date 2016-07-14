c11d154673ccb854f66e09789433d08c5cb352b7
Yesudeep Mangalapilly <yesudeep@google.com>
2016-07-14 10:09:04 -0700

    enable goimports

--- site-lisp/gemacs-go.el
+++ site-lisp/gemacs-go.el
@@ -7,9 +7,9 @@
 (autoload 'go-mode "go-mode" nil t)
 
 ;; Don't use goimports. We'll do it by hand.
-;; (eval-after-load 'go-mode
-;;   '(when (executable-find "goimports")
-;;      (setq gofmt-command "goimports")))
+(eval-after-load 'go-mode
+  '(when (executable-find "goimports")
+     (setq gofmt-command "goimports")))
 
 ;; Documentation.
 (require 'go-eldoc) ;; Don't need to require, if you install by package.el
